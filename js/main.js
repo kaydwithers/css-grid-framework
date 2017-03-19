4b59a7fb54996e3eddd978008b9fcb82d3ed9472
kaydwithers <kaydwithers@gmail.com>
2017-03-19 22:35:07 +1100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// Toggle
+Vue.component('v-toggle', {
+  template: '#v-toggle',
+  props: ['name'],
+  data: function() { 
+    return { 
+      toggleActive: false
+    }
+  },
+  methods: {
+    toggle: function() {
+      this.toggleActive = !this.toggleActive;
+    }
+  }
+})
+
+var app = new Vue({
+  el: '#v-app',
+  data: {
+    toggleActive: false,
+  }
+})
